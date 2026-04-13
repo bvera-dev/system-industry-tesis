a96734d0d6899baea75be98969a23fe3540a536b
Bryan Vera Lenis <bryan.veral@ug.edu.ec>
2026-04-13 07:42:40 -0500

    Conectar a base de datos postgres

--- config/settings.py
+++ config/settings.py
@@ -73,16 +73,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smri_test',
+        'USER': 'adm_monitor',
+        'PASSWORD': 'password.1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
